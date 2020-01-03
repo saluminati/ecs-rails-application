37f5e3488c81dbf91f9335b91258db6bdc5c17af
Salman Sohail <salmansohail@agilis-lab.com>
2020-01-04 03:41:05 +1100

    changed welcome message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-    render json: {"message": "Welcome"}
+    render json: {"message": "Welcome Aboard"}
     HomeJob.perform_later
   end
 end
